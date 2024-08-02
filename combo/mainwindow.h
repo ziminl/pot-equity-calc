dd194e802cbba8ad78b774618d11c0059c618eb4
crossrepublic <71635944+ziminl@users.noreply.github.com>
2024-08-02 19:39:15 +0900

    Create mainwindow.h

---
+++ combo/mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_calculateButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    double calculatePotEquity(int outs, double potSize, double betSize, int totalCards);
+    long long combination(int n, int k);
+};
+
+#endif // MAINWINDOW_H
